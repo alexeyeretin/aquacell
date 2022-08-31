7a9cf6af5fd2de958aad0e7b8792fccbae16bcc6
alexeyeretin <111900590+alexeyeretin@users.noreply.github.com>
2022-09-01 02:37:46 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector(".burger");
+/*const navMenu = document.querySelector("");*/
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	/*navMenu.classList.toggle("active");*/
+})
+
+/*document.querySelectorAll("").forEach(n => n._addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}))*/
