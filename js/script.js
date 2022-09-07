efff533c49eb971c263a734feb95e0646218f8a9
alexeyeretin <111900590+alexeyeretin@users.noreply.github.com>
2022-09-07 19:32:57 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const hamburger = document.querySelector(".burger");
-/*const navMenu = document.querySelector("");*/
+const navMenu = document.querySelector(".menu");
 
 hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
-	/*navMenu.classList.toggle("active");*/
+	navMenu.classList.toggle("active");
 })
 
-/*document.querySelectorAll("").forEach(n => n._addEventListener("click", () => {
+document.querySelectorAll(".nav_links_menu").forEach(n => n.addEventListener("click", () => {
 	hamburger.classList.remove("active");
 	navMenu.classList.remove("active");
-}))*/
+}))
